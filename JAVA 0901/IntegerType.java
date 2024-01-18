d7c3502f440205fe3481e392ba22b4559b9ba54d
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:26:23 -0300

    Update IntegerType.java

--- JAVA 0901/IntegerType.java
+++ JAVA 0901/IntegerType.java
@@ -1,6 +1,12 @@
+// declaração do objeto 'IntegerType'
 public class IntegerType {
     public static void main(String[] args) {
-        Integer intNumber = 2024;
-        System.out.println(intNumber);
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+
+        Integer intNumber = 2024; // criação da variável 'intNumber' do tipo 'Integer' com o valor de "2024"
+        System.out.println(intNumber); // exbição da variável acima no console
     }
 }
