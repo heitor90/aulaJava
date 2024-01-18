869ba844e5a9dc42e7cef6b8656a3ff0e43d57fc
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:55:30 -0300

    Update StringConcatenation.java

--- JAVA 0901/StringConcatenation.java
+++ JAVA 0901/StringConcatenation.java
@@ -1,5 +1,10 @@
+// declaração do objeto 'StringConcatenation'
 public class StringConcatenation {
     public static void main(String[] args) {
-        System.out.println(20 + "peças de sushi para" + 3 + "pessoas");       
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        System.out.println(20 + "peças de sushi para" + 3 + "pessoas"); // exibição do texto no console usando concatenação     
     }
 }
