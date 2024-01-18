d55ca159bba609f1adc30b6385cabfc825bbfd9d
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:51:35 -0300

    Update SearchInString.java

--- JAVA 0901/SearchInString.java
+++ JAVA 0901/SearchInString.java
@@ -1,10 +1,16 @@
+// declaração do objeto 'SearchInString'
 public class SearchInString {
    public static void main(String args[]) {
-        String heitor = "Meu nome é Heitor e eu tenho 20 anos";
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        String frase = "Meu nome é Heitor e eu tenho 20 anos"; // criação da variável 'frase' do tipo 'String' com o valor "Meu nome é
+       // Heitor e eu tenho 20 anos"
 
-        System.out.println(heitor);
+        System.out.println(frase); // exibição da variável no console
 
-        // o número que aparece o número do  caracter, que seria a posição da posição/letra
-        System.out.println(heitor.indexOf("anos"));
+        // o número que aparece no console, seria a posição da palavra/letra
+        System.out.println(heitor.indexOf("anos")); // exibição da posição da palavra desejada
    }
 }
