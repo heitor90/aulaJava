ee88870ca4b7f80a34fa07dc8e366a8055d19b93
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 13:55:22 -0300

    Update Teste.java

--- JAVA 0801/Teste.java
+++ JAVA 0801/Teste.java
@@ -1,9 +1,15 @@
+// declaração do objeto 'Teste' 
 public class Teste {
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+            // criação do método executor (main)
+            // static: este método não pode ser alterado não pode ser alterado
+            // void: não pode retornar nenhum valor
+            // String[] args: declaração de todos os paramêtros
+            System.out.println("Hello Word"); // exibição do texto "Hello Word" no console 
     }
 
+    // declaração de outro objeto estático do tipo 'String' com o nome 'ImprimirTextoMaiusculo' com parametro 'Texto'
     public static String ImprimirTextoMaiusculo(String Texto){
-        return Texto.toUpperCase();
+        return Texto.toUpperCase(); // retornando o valor 'Texto' que é 'Hello Word' e o transfornando em um texto com todas as letrar maiúsculas
     }
 }
