6a513eccc340dc15b6abafd521fd497793545607
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 13:42:16 -0300

    Update DoubleType.java

--- JAVA 0801/DoubleType.java
+++ JAVA 0801/DoubleType.java
@@ -1,9 +1,13 @@
-/**
- * DoubleType
- */
+
+// declaração do objeto 'DoubleType' do tipo publico
 public class DoubleType {
      public static void main(String[] args) {
-        Double doubleVar = 9.9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999;
-        System.out.println(doubleVar);
+           // criação do método executor (main)
+           // static: este método não pode ser alterado não pode ser alterado
+           // void: não pode retornar nenhum valor
+          
+        Double doubleVar = 9.999999999999999999999999999999999999999999999999999999999;
+          // declaração da variável 'doubleVar' do tipo 'Double
+        System.out.println(doubleVar); // exibição da variável 'doubleVar' no console 
      }
 }
