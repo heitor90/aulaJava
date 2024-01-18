ac2f8d701bb272f4f47700536765bac9f3c4f53b
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:20:29 -0300

    Update FloatType.java

--- JAVA 0901/FloatType.java
+++ JAVA 0901/FloatType.java
@@ -1,6 +1,12 @@
+// declaração do objeto 'FloatType'
 public class FloatType {
     public static void main(String[] args) {
-        Float floatNumb = 23.99f;
-        System.out.println(floatNumb);
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+
+        Float floatNumb = 23.99f; // criação da variável 'floatNumber' do tipo 'float' com o valor de "23.99"
+        System.out.println(floatNumb); // exibição da variável acima no console
     }
 }
