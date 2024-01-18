10407d43828a30d57c08c2a5d5b7e1d18ff03dba
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 13:59:27 -0300

    Update BooleanType.java

--- JAVA 0901/BooleanType.java
+++ JAVA 0901/BooleanType.java
@@ -1,7 +1,12 @@
 public class BooleanType {
-    public static void main(String[] args) {
-        Boolean Verdadeiro = true;
-        Boolean Falso = false;
+    public static void main(String[] args) { 
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        Boolean Verdadeiro = true; // criação da variável 'Verdadeiro' do tipo 'Boolean' com o valor de 'true' 
+        Boolean Falso = false; // criação da variável 'Falso' do tipo 'Boolean' com o valor de 'false'
         System.out.println("Este é o " + Verdadeiro + " e este é o " + Falso + ".");
+        // exibição no console o valor das duas variáveis criadas acima usando a concatenação '+'
     }
 }
