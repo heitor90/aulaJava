e5c25504556be297a04722e4e4c7a5415ec626fb
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:42:51 -0300

    Update MathSquare.java

--- JAVA 0901/MathSquare.java
+++ JAVA 0901/MathSquare.java
@@ -1,5 +1,10 @@
+// declaração do objeto 'MathSquare'
 public class MathSquare {
     public static void main(String[] args) {
-        System.out.println(Math.sqrt(144));
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        System.out.println(Math.sqrt(144)); // exibição da raíz quadrarda do número selecionado no console
     }
 }
