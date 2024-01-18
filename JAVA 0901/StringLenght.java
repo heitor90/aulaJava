e1991930d61bba8500a2981cc391849f36d4d726
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:59:53 -0300

    Update StringLenght.java

--- JAVA 0901/StringLenght.java
+++ JAVA 0901/StringLenght.java
@@ -1,6 +1,11 @@
+// declaração do objeto 'StringLenght'
 public class StringLenght {
     public static void main(String[] args) {
-        String name = "heitor";
-        System.out.println(name.length());
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        String name = "heitor"; // criação da variável 'name' do tipo 'String' com o valor "heitor"
+        System.out.println(name.length()); // exbição da variável porém com o tamanho da palavra selecionada (6)
     }
 }
