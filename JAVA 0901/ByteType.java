856193fb5d73528b80cf60c423c9a22d0d874fd2
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:00:48 -0300

    Update ByteType.java

--- JAVA 0901/ByteType.java
+++ JAVA 0901/ByteType.java
@@ -1,6 +1,11 @@
+// declaração do objeto 'ByteType'
 public class ByteType {
     public static void main(String[] args) {
-       byte byteNum = 43;
-       System.out.println(byteNum);
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+       byte byteNum = 43; // criação da variável 'byteNum' do tipo 'byte' com o valor de '43'
+       System.out.println(byteNum); // exibição do valor da variável no console
     }
 }
