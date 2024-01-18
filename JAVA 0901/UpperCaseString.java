ac4454c09da9b0a63106e4b400deb76a3e040206
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 15:03:34 -0300

    Update UpperCaseString.java

--- JAVA 0901/UpperCaseString.java
+++ JAVA 0901/UpperCaseString.java
@@ -1,7 +1,13 @@
+// declaração do obejto 'UpperCaseString'
 public class UpperCaseString {
     public static void main(String[] args) {
-        String nome = "HeiToR";
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        String nome = "HeiToR"; // criação da variável 'nome' do tipo 'String' com o valor "HeiToR"
 
-        System.out.println(nome.toUpperCase());
+        System.out.println(nome.toUpperCase()); // exibição da variável 'nome' com a utilização do 'toUpperCase' deixando todas a letras
+        // maiúsculas
     }
 }
