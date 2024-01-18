02b0333a88266f52ce8a5d6e60d5e11647ea0cac
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:35:34 -0300

    Update MathMax.java

--- JAVA 0901/MathMax.java
+++ JAVA 0901/MathMax.java
@@ -1,8 +1,13 @@
+// declaração do objeto 'MathMax'
 public class MathMax {
     public static void main(String[] args) {
-        double doubleNum = 23.000;
-        double doubleNum2 = 79.000;
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        double doubleNum = 23.000; // criação da variável 'doubleNum' do tipo 'double' com o valor "23.000"
+        double doubleNum2 = 79.000; // criação da variável 'doubleNum2' do tipo 'double' com o valor "79.000" 
 
-        System.out.println(Math.max(doubleNum, doubleNum2));
+        System.out.println(Math.max(doubleNum, doubleNum2)); // exibição das duas variáveis no console com o uso da biblioteca
     }
 }
