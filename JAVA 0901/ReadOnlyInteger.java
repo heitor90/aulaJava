f6e52cc4292f7df4e53d9ccad92a62858869a337
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:48:16 -0300

    Update ReadOnlyInteger.java

--- JAVA 0901/ReadOnlyInteger.java
+++ JAVA 0901/ReadOnlyInteger.java
@@ -1,6 +1,12 @@
+// declaração do objeto 'ReadOnlyInteger'
 public class ReadOnlyInteger {
     public static void  main(String[] args) {
-        final int Var = 100;
-        System.out.println(Var);
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        final int Var = 100; // criação de uma variável 'Var' do tipo int e também do tipo 'final', que a partir de agora essa
+        // variável NÃO poderá ser alterada enquanto ser 'final' com o valor "100", 
+        System.out.println(Var); // exibição da variável no console
       }
 }
