832a81e10b205803f951ef2cca9ab52bb6c79571
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:19:16 -0300

    Update DiferentVarTypes.java

--- JAVA 0901/DiferentVarTypes.java
+++ JAVA 0901/DiferentVarTypes.java
@@ -1,8 +1,14 @@
+// declaração do objeto 'DiferentVarTypes'
 public class DiferentVarTypes {
     public static void main(String[] args) {
-        String name = "heitor";
-        float num1 = 10.90f;
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
 
-        System.out.println(num1 + name);
+        String name = "heitor"; // criação da variável 'name' do tipo 'String' com o valor "Heitor"
+        float num1 = 10.90f; // criação da variável 'num1' do tipo 'float' com o valor "10.90"
+
+        System.out.println(num1 + name); // exibição das duas variáveis concatenadas no console
     }
 }
