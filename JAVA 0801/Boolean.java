6eabdd27c11f183089408783987a479962d1a8b7
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 13:56:03 -0300

    Update Boolean.java

--- JAVA 0801/Boolean.java
+++ JAVA 0801/Boolean.java
@@ -5,6 +5,7 @@ public class Boolean {
         // criação do método executor (main)
         // static: este método não pode ser alterado não pode ser alterado
         // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
         boolean VouF = true; // declaração da variável 'VouF' do tipo de 'boolean' com o valor de 'true'
         System.out.println(VouF); // exibição da váriavel no console
     }
