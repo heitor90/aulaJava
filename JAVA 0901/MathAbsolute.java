5885ec32e41d9557a367044505df668ae5dcfefc
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:33:09 -0300

    Update MathAbsolute.java

--- JAVA 0901/MathAbsolute.java
+++ JAVA 0901/MathAbsolute.java
@@ -1,19 +1,24 @@
+// importação da biblioteca 'Math'
 import java.lang.Math;
-// biblioteca
-
+// declaração do objeto 'MathAbsolute'
 public class MathAbsolute {
     public static void main(String[] args) {
-        double num = -9;
-        int num2 = -8;
-        float num3 = -7;
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
 
-        System.out.println("Sem biblioteca: " + num);
-        System.out.println("Com biblioteca: " + (Math.abs(num)));
+        double num = -9; // criação da variável 'num' do tipo 'double' com o valor de "-9"  
+        int num2 = -8; // criação da variável 'num2' do tipo 'int' com o valor de "-8"
+        float num3 = -7; // criação da variável 'num3' do tipo 'float' com o valor de "-7" 
 
-        System.out.println("Sem biblioteca: " + num2);
-        System.out.println("Com biblioteca: " + (Math.abs(num2)));
+        System.out.println("Sem biblioteca: " + num); // exibição da variável concatenada sem o uso da biblioteca
+        System.out.println("Com biblioteca: " + (Math.abs(num))); // exibição da variável concatenada usando a biblioteca
 
-        System.out.println("Sem biblioteca: " + num3);
-        System.out.println("Com biblioteca: " + (Math.abs(num3)));
+        System.out.println("Sem biblioteca: " + num2); // exibição da variável concatenada sem o uso da biblioteca
+        System.out.println("Com biblioteca: " + (Math.abs(num2))); // exibição da variável concatenada usando a biblioteca
+
+        System.out.println("Sem biblioteca: " + num3); // exibição da variável concatenada sem o uso da biblioteca
+        System.out.println("Com biblioteca: " + (Math.abs(num3))); // exibição da variável concatenada usando a biblioteca
     }
 }
