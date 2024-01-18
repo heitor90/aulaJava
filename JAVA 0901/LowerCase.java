3c90ef25c1381f81d98f8acaa75c51925483095b
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:28:20 -0300

    Update LowerCase.java

--- JAVA 0901/LowerCase.java
+++ JAVA 0901/LowerCase.java
@@ -1,7 +1,13 @@
+// declaração do objeto 'LowerCase'
 public class LowerCase {
     public static void main(String[] args) {
-        String name = "HEITOR";
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
 
-        System.out.println(name.toLowerCase());
+        String name = "HEITOR"; //  criação da variável 'name' do tipo 'String' com o valor de "Heitor"
+
+        System.out.println(name.toLowerCase()); // exibição da variável acima usando o 'toLowerCase' para transformar todas as letras para minúsculas
     }
 }
