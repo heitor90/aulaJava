12304d6f30ee3d0bbffc72e8a138bcdf39e70b54
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:54:11 -0300

    Update StringAndIntegerConcatenation.java

--- JAVA 0901/StringAndIntegerConcatenation.java
+++ JAVA 0901/StringAndIntegerConcatenation.java
@@ -1,6 +1,12 @@
+// declaração do objeto 'StringAndIntegerConcatenation'
 public class StringAndIntegerConcatenation {
+    
     public static void main(String[] args) {
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
         
-        System.out.println("Ele tem"+ 20 +" anos");
+        System.out.println("Ele tem"+ 20 +" anos"); // exibição do texto no console usando a concatenação
     }
 }
