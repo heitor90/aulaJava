fd7975de1d29ff6853d73e901abbc8abd62c5603
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 15:01:16 -0300

    Update StringType.java

--- JAVA 0901/StringType.java
+++ JAVA 0901/StringType.java
@@ -1,6 +1,11 @@
+// declaração do objeto 'StringType'
 public class StringType {
     public static void main(String[] args) {
-        String nome = "heitor";
-        System.out.println(nome);
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        String nome = "heitor"; // criação da variável 'nome' do tipo 'String' com o valor "heitor"
+        System.out.println(nome); // exibição da variável 'nome'
     }
 }
