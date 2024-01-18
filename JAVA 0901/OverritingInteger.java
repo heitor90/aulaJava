f984d518da5ce92244773f8798139ee7c4aabd2b
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:45:51 -0300

    Update OverritingInteger.java

--- JAVA 0901/OverritingInteger.java
+++ JAVA 0901/OverritingInteger.java
@@ -1,8 +1,13 @@
+// declaração do objeto 'OverritingInteger'
 public class OverritingInteger {
     public static void main(String[] args) {
-       int n1 = 2;
-       System.out.println(n1);
-       n1 = 30;
-       System.out.println(n1);
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+       int n1 = 2; // criação da variável 'n1' do tipo 'int' com o valor "2"
+       System.out.println(n1); // exibição da variável 'n1' no console
+       n1 = 30; // declarando um outro valor para a mesma variável
+       System.out.println(n1); // exibição da mesma variável porém com o valor sobrescrito
     }
 }
