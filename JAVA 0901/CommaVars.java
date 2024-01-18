89603aa283164324ab04043b44907d4760734e28
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:05:51 -0300

    Update CommaVars.java

--- JAVA 0901/CommaVars.java
+++ JAVA 0901/CommaVars.java
@@ -1,7 +1,13 @@
+// declaração do objeto 'CommaVars'
 public class CommaVars {
     public static void main(String[] args) {
-        String nome = "heitor", sobrenome = "Ferreira";
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        String nome = "Heitor", sobrenome = "Ferreira"; // criação de duas variáveis(nome; sobrenome) com os valores "Heitor" e "Ferreira" em um mesmo dado
+        // usando a ','
         
-        System.out.println(nome + sobrenome);
+        System.out.println(nome + sobrenome); // exibição das duas variáveis usando concatenação '+'
     }
 }
