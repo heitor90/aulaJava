b3ca74de2037c394b82d0b63027bd5383c70e48d
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:40:50 -0300

    Update MathRandom.java

--- JAVA 0901/MathRandom.java
+++ JAVA 0901/MathRandom.java
@@ -1,7 +1,12 @@
+// declaração do objeto 'MathRandom'
 public class MathRandom {
     public static void main(String[] args) {
-        double randomNum = Math.random();
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        double randomNum = Math.random(); //criação da variável 'randonNum' do tipo double 
 
-        System.out.println(randomNum);
+        System.out.println(randomNum); // vai gerar no um número aleatório e vai mostra-lo no console
     }
 }
