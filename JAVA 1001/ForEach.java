5a8633e1ecef4a6edd66340d44be18ecfcec20d2
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 15:23:24 -0300

    Update ForEach.java

--- JAVA 1001/ForEach.java
+++ JAVA 1001/ForEach.java
@@ -1,11 +1,19 @@
+// declaração objeto 'ForEach'
 public class ForEach {
     public static void main(String[] args) {
-        String[] frutas = {"Maçâ","Banana","Pera","Uva","Manga","Melancia"};
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        String[] frutas = {"Maçâ","Banana","Pera","Uva","Manga","Melancia"}; // criação de uma 'ArrayList' com o nome frutas do tipo 'String'
+        // com 6 itens
 
-        System.out.println("Você tem [" + frutas.length + "]");
+        System.out.println("Você tem [" + frutas.length + "]"); // exibe na tela uma mensagem de quantas frutas existem na lista com o 
+        // comando '.lenght' que irá fazer a contagem dos itens dentro da lista e irá exibi-la no console
 
-        for(int i=0; i < frutas.length; i++){
-            System.out.println("Fruta: " + frutas[i]);
+        for(int i=0; i < frutas.length; i++){ // criação do loop 'for', inicia uma variável 'i' do tipo 'int' com o valor "0";
+            // enquanto 'i' for menor que 'frutas.length'(total de frutas dentro da lista); adicione 1.
+            System.out.println("Fruta: " + frutas[i]); // exibindo o nome das frutas contidas na lista
         }
     }
 }
