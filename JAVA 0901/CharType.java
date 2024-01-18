ec1ee28ef640870363b8f658d4b0ce8a35106cec
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 14:02:19 -0300

    Update CharType.java

--- JAVA 0901/CharType.java
+++ JAVA 0901/CharType.java
@@ -1,6 +1,11 @@
+// declaração do objeto 'CharType'
 public class CharType {
     public static void main(String[] args) {
-        char Letra = 'V';
-        System.out.println(Letra);
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        char Letra = 'V'; // criação da variável 'Letra' do tipo char com o valor de 'V'
+        System.out.println(Letra); // exibição da variável 'Letra' no console 
     }
 }
