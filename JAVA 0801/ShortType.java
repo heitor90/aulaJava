e598065f38d3aaef69a453d7e06473859e838a59
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-18 13:55:33 -0300

    Update ShortType.java

--- JAVA 0801/ShortType.java
+++ JAVA 0801/ShortType.java
@@ -4,7 +4,7 @@ public class ShortType {
         // criação do método executor (main)
         // static: este método não pode ser alterado não pode ser alterado
         // void: não pode retornar nenhum valor
-
+        // String[] args: declaração de todos os paramêtros
         Short num = 10; // declaração da variável 'num' do tipo 'Short'
         System.out.println(num); // exibição da variável 'num' no console
     }
