759701c18a50e671329962237327abf4cc2cf599
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-19 16:26:38 -0300

    Update UpdateMySQL.java

--- JAVA 1601/UpdateMySQL.java
+++ JAVA 1601/UpdateMySQL.java
@@ -1,20 +1,36 @@
+// importação das bibliotecas
 import java.util.*;
 import java.sql.*;
-
+// declaração do objeto 'UpdateMySQL'
 public class UpdateMySQL {
     public static void main(String[] args) {
-            String status;
-        
+        String status;
+
+        try {
+            // Conexão com o banco de dados
             Connection conn = App.conectar();
+
+            // Scanner para entrada do usuário
             Scanner scBusca = new Scanner(System.in);
             System.out.println("Digite o seu login para verificar as informações: ");
             String strBusca = scBusca.nextLine();
+
+            // Construção da consulta SQL de UPDATE
             String strSqlUpdate = "UPDATE `mysql_connector`.`tbl_login` SET `login` = '" + strBusca + "';";
+
+            // Criação do Statement e execução da consulta UPDATE
             Statement stmSql = conn.createStatement();
-            ResultSet rsSql = stmSql.executeQuery(strSqlUpdate);
-            String nomes = "";
-            while (rsSql.next()) {
-                nomes += "[" + rsSql.getString("nome") + "] ";
-            }
+            int rowsAffected = stmSql.executeUpdate(strSqlUpdate);
+
+            // Exibição do número de linhas afetadas pela atualização
+            System.out.println("Número de linhas afetadas: " + rowsAffected);
+
+            stmSql.close(); // Fechamento do 'statement'
+            scBusca.close(); // Fechamento do scanner
+
+        } catch (Exception e) {
+            // Tratamento de exceções
+            System.out.println("Ops! Ocorreu o erro " + e);
         }
     }
+}
