4ef491ed4af01fed91339b6fc2b1ca516d2356fa
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-19 14:37:31 -0300

    Update App.java

--- JAVA 1201/App.java
+++ JAVA 1201/App.java
@@ -1,30 +1,38 @@
+// importação das bibliotecas usadas
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// declaração do objeto 'App'
 public class App {
     public static void main(String[] args) {
-        System.out.println("Conectando ao banco de dados");
-        conectar();
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+        System.out.println("Conectando ao banco de dados"); // mensagem mostrada no console
+        conectar(); // invocando o método 'conectar'
     }
-    public static Connection conectar(){
-        String status = "Nada aconteceu ainda";
+    public static Connection conectar(){ // criação de outro método público, estático, do tipo 'Connection' com o nome 'conectar' 
+        // criação das variáveis para configurar conexão com o banco de dados MySQL
+        String status = "Nada aconteceu ainda"; // variável que será exibida no final 
         String mysqlHost = "127.0.0.1";
         String mysqlDb = "mysql_connector";
         String mysqlUser = "heitor";
         String mysqlPassword = "";
         String mysqlUrl = "jdbc:mysql://" + mysqlHost + "/" + mysqlDb + "?user=" + mysqlUser + "&password=" + mysqlPassword;
         
-        Connection conn = null;
+        Connection conn = null; // objeto 'Connection' que representará a conexão ao banco de dados
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            conn = DriverManager.getConnection(mysqlUrl);
-            status = "Conexão realizadas com sucesso";
+        try { // iníco do bloco 'try'
+            Class.forName("com.mysql.jdbc.Driver").newInstance(); // carregamento do driver JDBC para MySQL
+            conn = DriverManager.getConnection(mysqlUrl); // variável 'conn' realiza a conexão com o banco com os dados da variável 'mysqlUrl'
+            status = "Conexão realizadas com sucesso"; // atualização do valor do 'status' para ser exibida no console
         } catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            // captura de exceções em caso de falha e atualização do status com a mensagem de erro
             status = "Ops! Algo de errado não está certo " + e;
         }
-        System.out.println(status);
-        return conn;
+        System.out.println(status); // mensagem mostrada no console
+        return conn; // retorno da 'Connection'
     }
 }
