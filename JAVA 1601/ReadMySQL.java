dc3b1135cc0ff8149161ad9d931b4171ab41a545
heitor90 <98494754+heitor90@users.noreply.github.com>
2024-01-19 16:24:44 -0300

    Update ReadMySQL.java

--- JAVA 1601/ReadMySQL.java
+++ JAVA 1601/ReadMySQL.java
@@ -4,6 +4,11 @@ import java.util.*;
 // declaração do objeto 'ReadMySQL'
 public class ReadMySQL {
     public static void main(String[] args) {
+        // criação do método executor (main)
+        // static: este método não pode ser alterado não pode ser alterado
+        // void: não pode retornar nenhum valor
+        // String[] args: declaração de todos os paramêtros
+
         // Inicialização da variável de status
         String status = "Nada aconteceu ainda...";
 
